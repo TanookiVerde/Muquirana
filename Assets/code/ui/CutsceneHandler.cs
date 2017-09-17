077a29aa3d40ab0750b495877a12d0115022f730
Lucas <lucas.cerqueira@poli.ufrj.br>
2017-09-17 16:56:09 +0100

    Começo da implementação de cutscene

---
+++ Assets/code/ui/CutsceneHandler.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneHandler : MonoBehaviour {
+
+	[SerializeField] private float cutsceneTime = 2f;
+
+	[Header("Fade preferences")]
+	[SerializeField] private GameObject fadePanel;
+	[SerializeField] private float fadeTime = 1f;
+
+	private FadeInOut fadeScript;
+
+	private List<GameObject> cutsceneList = new List<GameObject>();
+	private int currentCutscene = 0;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		fadeScript = fadePanel.GetComponent<FadeInOut> ();
+
+		// Add cutscene objects to a list and disable all of them
+		for (int i = 0; i < transform.childCount; i++) 
+		{
+			GameObject obj = transform.GetChild (i).gameObject;
+			cutsceneList.Add (obj);
+			obj.SetActive (false);
+		}
+
+		StartCoroutine (HandleCutscenes ());
+	}
+
+
+	private IEnumerator HandleCutscenes ()
+	{
+		// Enable the first cutscene and fade out
+		cutsceneList [0].SetActive (true);
+		fadeScript.StartCoroutine (fadeScript.FadeOut (fadeTime));
+
+	}
+
+	private IEnumerator ChangeCutscene ()
+	{
+		// Fade in
+		yield return fadeScript.FadeIn (fadeTime);
+		yield return new WaitForSeconds (fadeTime);
+
+		// Change cutscene
+		cutsceneList[currentCutscene].SetActive (false);
+		currentCutscene++;
+		cutsceneList [currentCutscene].SetActive (true);
+
+		//Fade out
+		yield return fadeScript.FadeOut (fadeTime);
+		yield return new WaitForSeconds (fadeTime);
+	}
+}
---
+++ Assets/code/ui/FadeInOut.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class FadeInOut : MonoBehaviour {
+
+	private Image panelImage;
+
+	// Use this for initialization
+	void Start () 
+	{
+		panelImage = GetComponent<Image> ();
+	}
+
+	// Escurecer
+	public IEnumerator FadeIn (float time)
+	{
+		panelImage.CrossFadeAlpha (255, time, false);
+		yield return new WaitForSeconds (time);
+	}
+
+	// Clarear
+	public IEnumerator FadeOut (float time)
+	{
+		panelImage.CrossFadeAlpha (0.001f, time, false);
+		yield return new WaitForSeconds (time);
+	}
+}
